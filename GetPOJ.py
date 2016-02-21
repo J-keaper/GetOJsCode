249a60e1eb9e5422de9cbc6d781c01b21fdda251
NUC-Edward <1273570695@qq.com>
2016-02-21 19:36:09 +0800

    Update GetPOJ.py

--- GetPOJ.py
+++ GetPOJ.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 import urllib
 import urllib2
 import cookielib
@@ -16,14 +11,14 @@ import os
 
 html_parser=HTMLParser.HTMLParser()
 url="http://poj.org/"
+#登陆并保存cookie
 def login(id,password):
     data=urllib.urlencode({'user_id1':id,'password1':password,'B1':'login','url':'/'})
     login_url="http://poj.org/login"
     cookie=cookielib.CookieJar()
     opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
     urllib2.install_opener(opener)
-    #headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36"}
-    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}    
+    headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36"}
     request=urllib2.Request(login_url,data,headers)
     response=urllib2.urlopen(request).read()
     #print response
@@ -31,7 +26,7 @@ def login(id,password):
         return True
     else:
         return False
-        
+#传入runid取出代码
 def getcode(runid):
     code_url="http://poj.org/showsource?solution_id="+runid
     code_page=urllib2.urlopen(code_url).read().decode('utf-8')
@@ -39,8 +34,8 @@ def getcode(runid):
     ans=re.search(pattern,code_page)
     code=ans.group(2)
     #print chardet.detect(code)
-    code=html_parser.unescape(code)
-    code_type=ans.group(1)
+    code=html_parser.unescape(code)#格式化html标签，如标点符号等
+    code_type=ans.group(1)#代码后缀
     code_type=re.sub("sh_",".",code_type)
     #print code,code_type
     return (code,code_type)
@@ -48,6 +43,7 @@ def getcode(runid):
 def gettitle(pro_id):
     pro_url="http://poj.org/problem?id="+str(pro_id)
     pro_page=urllib2.urlopen(pro_url).read().decode('utf-8')
+    #访问限制
     while "Please retry after 5000ms.Thank you." in pro_page:
         time.sleep(6)
         pro_page=urllib2.urlopen(pro_url).read().decode('utf-8')
@@ -64,8 +60,8 @@ def run(id,password,dirpath):
         return False
     print "Login success!"
     os.chdir(dirpath)
-    status_url="http://poj.org/status?problem_id=&user_id="+id+"&result=0&language="
-    #status_url="http://poj.org/status?user_id=jiajiawang&result=0&top=14838759"  
+    #status页面
+    status_url="http://poj.org/status?problem_id=&user_id="+id+"&result=0&language="  
     while True:    
         page=urllib2.urlopen(status_url).read().decode('utf-8')
         #print page
@@ -81,20 +77,21 @@ def run(id,password,dirpath):
             title=gettitle(pro_id)
             code=getcode(run_id)
             pro_name=pro_id+' '+title+code[1]
-            print pro_name
             code_con=code[0].encode('utf-8')
             code_con=code_con.replace('\r\n','\n')
+            print u"正在保存"+pro_name+"..."            
             f=open(pro_name,"w")
             f.write(code_con)            
             f.close()
+            print u"保存"+pro_name+u"完成。"
             #time.sleep(2)
         status_url=url+re.search(r"Previous Page.*?\[<a href=(.*?)>",page).group(1)
-        print status_url
+        #print status_url
         
         
 id=raw_input("Please input your User ID:")
 password=getpass.getpass("Please input your password:")
 dirpath=raw_input("Please input direetory:")
 run(id,password,dirpath)
-#run("jiajiawang","******","E:\poj")
+#run("jiajiawang","*****","E:\poj")
 
